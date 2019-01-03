301cd6d14953019958877be65410f29da6effe34
Dani Castilho <danicastilhodev@gmail.com>
2019-01-03 01:01:26 -0200

    Create some modules

---
+++ src/button-style.js
@@ -0,0 +1,7 @@
+const red = "color: red;";
+const blue = "color: blue;";
+const MakeColorStyle = (color) => `color: ${color}`
+
+exports.red = red;
+exports.blue = blue;
+exports.MakeColorStyle = MakeColorStyle;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import nav from "./nav";
+import {top, bottom} from "./footer";
+import makeButton from "./button";
+import {red, blue, makeColorStyle} from "./button-style"
+console.log(nav(), top, bottom, makeButton("MyButton"), makeColorStyle(red));
